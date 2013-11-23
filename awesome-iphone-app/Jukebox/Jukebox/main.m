5d1ade42f5da9abe1c929c2bedbf0c50de7fe452
Stefan Filip <stefan@nextroot.com>
2013-11-23 10:53:43 +0200

    Initial Commit

---
+++ awesome-iphone-app/Jukebox/Jukebox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jukebox
+//
+//  Created by Stefan Filip on 23/11/13.
+//  Copyright (c) 2013 Hack Attack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JBAppDelegate class]));
+    }
+}
