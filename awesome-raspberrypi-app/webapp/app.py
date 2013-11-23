0c4dc490662944f13efe479077ecc1d384c87474
Horia Jurcut <horia.dragos@gmail.com>
2013-11-23 14:07:21 +0200

    API init

--- awesome-raspberrypi-app/webapp/app.py
+++ awesome-raspberrypi-app/webapp/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+from flask.ext.sqlalchemy import SQLAlchemy
+from settings import settings
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = settings['SQLALCHEMY_DATABASE_URI']
+db = SQLAlchemy(app)
+
+from models import User
+from models import Song
+
+@app.route("/browse")
+def browse():
+  return jsonify()
 
 if __name__ == "__main__":
-  app.run()
+  app.run(host='0.0.0.0')
